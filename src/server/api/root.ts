7a4a469efbc8053d415046af7b3111dd705a6754
Boris Martinovic <boris@martinovic.dev>
2023-01-07 12:56:01 +0100

    Index + calculation

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
+import {calculationRouter} from "./routers/calculation";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { exampleRouter } from "./routers/example";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  calculateRest: calculationRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/calculation.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+import {createTRPCRouter, publicProcedure} from "../trpc";
+import {formatNumberToString} from "../../../utils/numberOperations";
+const rate = 7.5345;
+export const calculationRouter = createTRPCRouter({
+  calculateRest: publicProcedure
+    .input(
+      z.object({
+        priceEur: z.number().nonnegative(),
+        givenEur: z.number().nonnegative(),
+        givenKn: z.number().nonnegative(),
+      })
+    )
+    .query(({ input }) => {
+      const returnSum = input.priceEur - input.givenEur - input.givenKn / rate;
+      const returnSumEur = formatNumberToString(
+        Math.abs(Math.round(returnSum * 100) / 100)
+      );
+      const returnSumKn = formatNumberToString(
+        Math.abs(Math.round(returnSum * rate * 100) / 100)
+      );
+
+      return {
+        resultText:
+          returnSum <= 0
+            ? `Preostalo za vratiti kupcu: ${returnSumEur} €, tj. ${returnSumKn} kn`
+            : `Kupac duguje: ${returnSumEur} €, tj. ${returnSumKn} kn`,
+      };
+    }),
+});
